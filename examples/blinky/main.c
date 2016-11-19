e15c151bf30c0386d8fb0d6d7ac3c32722ad23d6
Olaf Bergmann <bergmann@tzi.org>
2016-11-19 16:02:11 +0100

    Blinky example used to test BOARD=bluepill
    Toggles the internal LED.

---
+++ examples/blinky/main.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2016 TZI Uni Bremen
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Blinky application
+ *
+ * @author      Olaf Bergmann <bergmann@tzi.org>
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include "board.h"
+#include "xtimer.h"
+
+int main(void)
+{
+    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
+    while (1) {
+      xtimer_usleep(1000000UL);
+      LED0_TOGGLE;
+    }
+    return 0;
+}
